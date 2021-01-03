524962bc1faef97d913af4d6107334b0012ff7af
Jangulo7 <johanna.angulo@gmail.com>
2021-01-03 15:00:25 -0300

    refactor: Customization of HTML & CSS layout. Collapsable sections in Javascript.

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -55,3 +55,24 @@
 // Set sections as active
 
 
+/* Collapsable Sections */
+const acc = document.getElementsByClassName('accordion');
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function() {
+      const newAcc = this;
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    newAcc.classList.toggle('active');
+    /* Toggle between hiding and showing the active panel */
+    const panel = newAcc.nextElementSibling;
+    if (panel.style.display === 'none') {
+      panel.style.display = 'block';
+    } else {
+      panel.style.display = 'none';
+      newAcc.classList.toggle('active');
+    }
+  });
+}
+
