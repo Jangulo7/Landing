c3055507c218ea28e74457b599397e46bf2024e0
Jangulo7 <johanna.angulo@gmail.com>
2021-01-10 01:15:58 -0300

    refactor: New functionality. A form was added in a new sidebar to input data. The data is shown in the DOM and the navbar is dynamically updated.  The form webpage was deleted since the form was added to the index page in the sidebar.

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -17,7 +17,19 @@
  * Define Global Variables
  * 
 */
+const form = document.getElementById('addForm');
+const navList = document.getElementById('navbar__list');
+  
+  //Create new html to insert new elements
+  const newSection = document.querySelector('#cont');
+  const htmlTextToAdd = '<section><div class="landing__container"><div class="accordion"><h3 id="newTitle"></h3></div><div id="newContent" class="panel"></div></div></section>';
+  newSection.insertAdjacentHTML('beforeend', htmlTextToAdd);
 
+  //Select location to insert section title
+  const insertTitle = document.getElementById('newTitle');
+
+  //Select location to insert section content
+  const insertContent = document.getElementById('newContent');
 
 /**
  * End Global Variables
@@ -27,13 +39,53 @@
 
 
 
+
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
 
-// build the nav
+
+// Build the nav
+
+// Form submit event
+form.addEventListener('submit', addSection);
+
+// Add a Section Title to the navbar and add the section text & author to the main content
+function addSection(e){
+  e.preventDefault();
+
+  // Get input values
+  const newAuthor = document.getElementById('name').value;
+  const newTitle = document.getElementById('title').value;
+  const newText = document.getElementById('section').value;
+
+  // Create new li element in the navbar
+  const li = document.createElement('li');
+
+  // Add class new elements
+  li.className = 'active';
+
+  // Create a new p element in the content area
+  const p = document.createElement('p');
+
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newTitle));
+  p.appendChild(document.createTextNode(newText));
+
+  // Append li to navbar
+  navList.appendChild(li);
+
+  //Add title to the section title
+  insertTitle.textContent = newTitle;
+
+  //Add section text to the content area
+  insertContent.appendChild(p);
+
+  
+}
+
 
 
 // Add class 'active' to section when near top of viewport
